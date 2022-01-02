793f3e3446925680d8ecb49a17be6ce5e3a4f07f
adomini15 <adomini.edu@gmail.com>
2022-01-02 00:03:54 -0400

    Configuration changes applied

--- app.js
+++ app.js
@@ -1,80 +1,84 @@
 // *** dependencies settings and imports
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // *** constants
 
-const SERVER_PORT = process.env.SERVER_PORT
-const dbURI = process.env.DB_URI
-
+const port = process.env.PORT || 3000;
+const dbURI = process.env.DB_URI;
 
 // *** app
-const app = express()
+const app = express();
 
 // preconfig mongoose
-mongoose.set('runValidators', true)
+mongoose.set("runValidators", true);
 
 // *** connect to atlas mongodb
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(() => {
-  app.listen(SERVER_PORT)
-}).catch((err) => {
-  console.log(err)
-})
-
+mongoose
+	.connect(dbURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		app.listen(port);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 // *** routes imports
 
 // User Context
-const userRoute = require('./routes/User/user.route')
-const authRoute = require('./routes/User/auth.route')
-const profileRoute = require('./routes/User/profile.route')
-const roleRoute = require('./routes/User/role.route')
-const actionRoute = require('./routes/User/action.route')
+const userRoute = require("./routes/User/user.route");
+const authRoute = require("./routes/User/auth.route");
+const profileRoute = require("./routes/User/profile.route");
+const roleRoute = require("./routes/User/role.route");
+const actionRoute = require("./routes/User/action.route");
 
 // Student Context
-const studentRoute = require('./routes/Student/student.route')
-const fatherRoute = require('./routes/Student/father.route')
-const motherRoute = require('./routes/Student/mother.route')
-const tutorRoute = require('./routes/Student/tutor.route')
+const studentRoute = require("./routes/Student/student.route");
+const fatherRoute = require("./routes/Student/father.route");
+const motherRoute = require("./routes/Student/mother.route");
+const tutorRoute = require("./routes/Student/tutor.route");
 
 // Course Context
-const courseRoute = require('./routes/Course/course.route')
+const courseRoute = require("./routes/Course/course.route");
+
+// proxy (important for heroku in production)
+app.set("trust proxy", 1);
 
 // cors
 
-// app.use(cors({
-//   origin: 'http://localhost:3001',
-//   credentials: true
-// }))
+app.use(
+	cors({
+		credentials: true,
+		origin: process.env.NODE_ENV == "production" ? "" : "http://localhost:3001",
+	})
+);
 
 // cookie-parser
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(express.json())
+app.use(express.json());
 
 // morgan
-app.use(morgan('dev'))
-
+app.use(morgan("dev"));
 
 // *** routes
 
-app.use('/users', userRoute)
-app.use(authRoute)
-app.use('/profiles', profileRoute)
-app.use('/roles', roleRoute)
-app.use('/actions', actionRoute)
-app.use('/students', studentRoute)
-app.use('/fathers', fatherRoute)
-app.use('/mothers', motherRoute)
-app.use('/tutors', tutorRoute)
-app.use('/courses', courseRoute)
+app.use("/users", userRoute);
+app.use(authRoute);
+app.use("/profiles", profileRoute);
+app.use("/roles", roleRoute);
+app.use("/actions", actionRoute);
+app.use("/students", studentRoute);
+app.use("/fathers", fatherRoute);
+app.use("/mothers", motherRoute);
+app.use("/tutors", tutorRoute);
+app.use("/courses", courseRoute);
--- controllers/User/auth.controller.js
+++ controllers/User/auth.controller.js
@@ -1,40 +1,47 @@
 // model
-const User = require('../../models/User/User')
+const User = require("../../models/User/User");
 
 // utils
-const handleErrors = require('../../Utils/Errors/handleErrors')
-const { createToken } = require('../../Utils/Auth/Auth')
+const handleErrors = require("../../Utils/Errors/handleErrors");
+const { createToken } = require("../../Utils/Auth/Auth");
 
 module.exports.signup = async (req, res) => {
-  try {
-    const user = await User.create(req.body) 
-    
-    res.status(200).json(user)
-  } catch (err) {
-    const errors = handleErrors(err)
-    res.status(400).json({errors})
-  }
-}
+	try {
+		const user = await User.create(req.body);
+
+		res.status(200).json(user);
+	} catch (err) {
+		const errors = handleErrors(err);
+		res.status(400).json({ errors });
+	}
+};
 
 module.exports.login = async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  try {
-    const user = await User.login(email, password) 
+	try {
+		const user = await User.login(email, password);
 
-    const token = createToken(user._id)
-    res.cookie('jwt', token, {
-      maxAge: 1000 * 60 * 60 * 24 * 3,
-    })
+		const token = createToken(user._id);
 
-    res.status(200).json({ok: 1, message: 'login done correctly'})
-  } catch (err) {
-    const errors = handleErrors(err)
-    res.status(400).json({errors})
-  }
-}
+		res.cookie("jwt", token, {
+			maxAge: 1000 * 60 * 60 * 24 * 3,
+			httpOnly: true,
+			secure: process.env.NODE_ENV == "production",
+		});
+
+		res.status(200).json({ ok: 1, message: "login done correctly" });
+	} catch (err) {
+		const errors = handleErrors(err);
+		res.status(400).json({ errors });
+	}
+};
 
 module.exports.logout = (req, res) => {
-  res.cookie('jwt','', { maxAge: 1})
-  res.status(200).json({ ok: 1, message: 'logout done correctly'})
-}
+	res.cookie("jwt", "", {
+		maxAge: 1,
+		httpOnly: true,
+		secure: process.env.NODE_ENV == "production",
+	});
+	res.status(200).json({ ok: 1, message: "logout done correctly" });
+};
